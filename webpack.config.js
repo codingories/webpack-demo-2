db03c24e777e319c8e74fa1b72fc17968cd19231
Ories Zhu <tellt@126.com>
2019-12-07 22:22:32 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 var path = require("path");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "[name].[contenthash].js"
+    filename: "index.[contenthash].js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "My App1",
+      template: "src/assets/index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+        // 用css-loader处理文件，把文件的内容读到js里面，style-loader，把css-loader读到的东西变成style标签放到header里
+      }
+    ]
   }
 };
